e377ade355d7abaa9d753e41a0d861f3ff91a461
Pete <pbianchi192@gmail.com>
2018-01-29 19:03:41 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var daysUntilMyBirthday = 60;
+
+while (daysUntilMyBirthday > 30) {
+    console.log (daysUntilMyBirthday + " days until my birthday, I wish my birthday was closer");
+    daysUntilMyBirthday --;
+} 
+while (daysUntilMyBirthday > 5) {
+    console.log (daysUntilMyBirthday + " days until my birthday, it's almost my birthday!");
+    daysUntilMyBirthday --;
+} 
+while (daysUntilMyBirthday > 0) {
+    console.log (daysUntilMyBirthday + " days until my birthday, HEEELLL YYEEEAAA!");
+    daysUntilMyBirthday --;
+}
+while (daysUntilMyBirthday == 0) {
+    console.log ("HAPPY BIRTHDAY!");
+    daysUntilMyBirthday --;
+}
